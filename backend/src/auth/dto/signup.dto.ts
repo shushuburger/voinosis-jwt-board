3fe484eb09acf350d62c7730121fc4cd2440f0eb
shushuburger <03robin@naver.com>
2026-06-20 20:21:25 +0900

    fix: handle auth exceptions

--- backend/src/auth/dto/signup.dto.ts
+++ backend/src/auth/dto/signup.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class SignupDto {
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsString()
   @MinLength(8)
   password: string;
